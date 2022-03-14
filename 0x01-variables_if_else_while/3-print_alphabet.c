5cb79acb65f0b88ffbcf07bd06925146418581e6
Bee095 <abintoy@gmail.com>
2022-03-14 21:55:02 +0300

    hotfix

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -8,11 +8,20 @@
 int main(void)
 {
         char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-	for (i = 0; i < 52; i++)
+        int 1;
+        for (i = 0; i < 52; i++)
         {
                 putchar(alp[i]);
         }
         putchar ('\n');
         return (0);
 }
+#include <stdio.h>
+
+/**
+ *main - Prints the alphabet.
+ *
+ *Return = Always (0) Success
+ */
+int main(void)
+{
