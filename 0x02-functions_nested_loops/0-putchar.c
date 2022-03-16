15a53d750041f9a693268690265ad0f807b69e23
Bee095 <abintoy@gmail.com>
2022-03-16 09:08:31 +0300

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,13 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * main - Check description.
- * Description - Prints the word _putchar followed by a new line.
- * Return: Always 0 (Success).
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
-        int count, sz;
-
-        sz = size of(str)/ size of(int)
-        for (count = 0, count < sz, count++)
-        {
-          _putchar(str[count]);
-        }
-        _putchar('\n');
-        return (0);
+	return (write(1, &c, 1));
 }
-
-	_putchar('\n');
-	return (0);
-}	
-
