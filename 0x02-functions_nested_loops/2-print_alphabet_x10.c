67727823a1b8af2bfa6602f3b22784aa7f4b5deb
Bee095 <abintoy@gmail.com>
2022-03-17 16:20:26 +0300

    Hotter fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabetx10(void)
 	char i;
 	char j;
 
-	for (j = 0; j < 10; j++)
+	for (j = 0; j <= 10; j++)
 	{ 
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
