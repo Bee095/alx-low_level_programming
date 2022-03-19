98246f11e0488a285f5b77c813e0d8f03fc93e9c
Bee095 <abintoy@gmail.com>
2022-03-19 11:39:11 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,24 @@
-#include <stdlib>
-include "main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints Buzz each numbers of 3 and 5.
  * Return: Always 0.
  */
-int i, n ;
-    printf("Enter the number :");
-    scanf("%d", &n);
-    for (i=1; i<=n; i++)
+int main(void)
+{
+    int i;
+    for(i=1; i<=100; ++i)
     {
-// number divisible by 3 and 5 will always be divisible by 15, print 'FizzBuzz' in place of the number
-        if (i%15 == 0)
-            printf ("FizzBuzz\t");
-
-// number divisible by 3, print 'Fizz' in place of the number
-        else if ((i%3) == 0)
-            printf("Fizz\t");
-
-// number divisible by 5, print 'Buzz' in place of the number
-        else if ((i%5) == 0)
-            printf("Buzz\t");
-
-        else // print the number
-            printf("%d\t", i);
+        if (i % 3 == 0)
+            printf("Fizz");
+        if (i % 5 == 0)
+            printf("Buzz");
+        if ((i % 3 != 0) && (i % 5 != 0))
+            printf("number=%d", i);
+        printf("\n");
     }
+
     return 0;
 }
+
